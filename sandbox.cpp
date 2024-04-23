617cbffaab4583c3f856ec5f160c8107ff7e6a39
Mika <matis.mikael@gmail.com>
2024-04-23 16:56:29 +0200

    first commit

---
+++ sandbox.cpp
@@ -0,0 +1,16 @@
+/**
+ * @file sandbox.cpp
+ * @author lost_in_nowhere
+ * @brief sandbox to try the code
+ * @version 0.1
+ * @date 2024-04-23
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+import smath;
+int main()
+{
+    return 0;
+}
